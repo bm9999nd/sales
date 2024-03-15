3bd6cf8f994db758d1781df7614ddb3e43626ddc
bm9999nd <bm9999nd@gmail.com>
2024-03-15 19:19:53 +0700

    Auto-generated commit

---
+++ main-6JAGMNYM.js
@@ -0,0 +1,1 @@
+import{fa as t,ga as p,ha as r,ia as n}from"./chunk-7Q7TVZLS.js";var m=[{path:"transaction",loadComponent:()=>import("./chunk-YDWB6PF5.js").then(o=>o.TransactionComponent)},{path:"items",loadComponent:()=>import("./chunk-PWG4KNRV.js").then(o=>o.ItemsComponent)}];var e={providers:[r(m),t()]};p(n,e).catch(o=>console.error(o));
