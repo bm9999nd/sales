f02b7bd91360d80b9d10d2dce6ef2eb84dde2ce2
nda_666 <bm9999nd@gmail.com>
2024-03-20 14:51:27 +0700

    Auto-generated commit

---
+++ chunk-6FDTVWM3.js
@@ -0,0 +1,1 @@
+import{$a as a,Ma as o,Na as r,Z as i,hb as p}from"./chunk-FZMQ4P4R.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-print"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,c){n&1&&(o(0,"p"),a(1,"print works!"),r())}});let t=e;return t})();export{d as PrintComponent};
